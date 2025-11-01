72e97b1865e5adbb51fbb23f7cbbbaa5183c63c0
chayalokesh2315 <chayalokesh237@gmail.com>
2025-11-01 23:15:41 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -23,7 +23,8 @@
       }
 
       try {
-        const res = await fetch(`${BASE_URL}/signup`, {
+        const res = await fetch("http://localhost:5000/api/auth/register", {
+ {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ name, email, password }),
