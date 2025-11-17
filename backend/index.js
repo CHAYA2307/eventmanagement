94ea5bd3fd4cfbb9305ba5999a2f23b8aa152dcf
chayalokesh2315 <chayalokesh237@gmail.com>
2025-11-17 09:40:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,5 @@ const userRoutes = require('./routes/user');
 app.use('/api/user', userRoutes);
 
 // Start server
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
